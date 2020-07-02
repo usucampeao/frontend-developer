fd9c028b44de9df197b4754feb8cf611c5faa1a5
matinhu <matheulls@hotmail.com>
2020-07-02 15:45:45 -0300

    O primeiro get a gente nunca esquece. Consumindo api com sucesso. [wip]

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/Pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
-  styleUrls: ['./pokedex.component.scss']
+  styleUrls: ['./pokedex.component.scss'],
 })
 export class PokedexComponent implements OnInit {
-
-
+  
   pokemons: Pokemon[] = [];
+  lstPokemon = [];
+  
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
 
-  constructor(private pokemonService: PokemonService) {
+  constructor(private pokemonService: PokemonService) {}
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.lstPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(filterValue));
   }
-
   ngOnInit(): void {
+
+    this.pokemonService.getTest().subscribe((res) => {
+      const list: any[] = [];
+
+      list.push(res.results);
+      this.lstPokemon = list[0];
+      console.log(this.lstPokemon);
+
+    });
+
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+
   }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class PokemonService {}
+export class PokemonService {
+  
+
+  constructor(private http: HttpClient) {
+  
+  }
+
+  getTest() {
+    return this.http.get<any>('https://pokeapi.co/api/v2/pokemon');
+  }
+
+  
+}
