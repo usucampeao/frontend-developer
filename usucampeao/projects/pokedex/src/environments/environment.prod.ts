7d098c26e24629bce7e0e33e1c3083bc444782a9
Ramon Conrado <ramonconrado94@gmail.com>
2020-07-29 03:20:06 -0300

    variaveis de ambiente

--- usucampeao/projects/pokedex/src/environments/environment.prod.ts
+++ usucampeao/projects/pokedex/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiEndpoint: "https://pokeapi.co/api/v2",
   production: true
 };
--- usucampeao/projects/pokedex/src/environments/environment.ts
+++ usucampeao/projects/pokedex/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  apiEndpoint: "https://pokeapi.co/api/v2",
   production: false
 };
 
