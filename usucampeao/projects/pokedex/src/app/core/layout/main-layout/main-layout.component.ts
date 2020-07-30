8d0fae5ca4d001f9c97bd8f5e1344097d3e0cc95
unknown <ramonconrado94@gmail.com>
2020-07-29 22:02:25 -0300

    implementação de tema dinamico

--- usucampeao/projects/pokedex/src/app/core/layout/main-layout/main-layout.component.ts
+++ usucampeao/projects/pokedex/src/app/core/layout/main-layout/main-layout.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
-
+  theme: string = 'orange'
   constructor() { }
 
   ngOnInit(): void {
+    let theme = localStorage.getItem('theme');
+    theme ? this.theme = theme : false
   }
 
+  setTheme(theme: string) {
+    this.theme = theme;
+    localStorage.setItem("theme", theme)
+  }
 }
