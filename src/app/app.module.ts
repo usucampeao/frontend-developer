d2ffd28941871e19a022d9dddf8ba3c98f60ed65
Matheus Cabral <matheuscabraloli@gmail.com>
2020-09-03 16:06:53 -0300

    declarations HttpClient e Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { MaterialModule } from './shared/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokeTableComponent,
+    PokeDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
