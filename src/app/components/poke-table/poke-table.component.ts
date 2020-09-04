7eac7814c3c6f7e66eefcb7ea2420c164b729c90
Matheus Cabral <matheuscabraloli@gmail.com>
2020-09-03 22:58:47 -0300

    151

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -26,7 +26,7 @@ export class PokeTableComponent implements OnInit {
   getPokemons() {
     let pokemonData;
 
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 151; i++) {
       this.pokeService.getPokemons(i).subscribe(
         res => {
           pokemonData = {
