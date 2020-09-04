d88ea18436d36cbabc4236bc1408070a32d53446
Matheus Cabral <matheuscabraloli@gmail.com>
2020-09-04 14:45:57 -0300

    Service getter

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,18 +2,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  baseUrl = environment.baseUrl;
+  // baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
-  getPokemons(index) {
-    console.log(this.baseUrl);
-    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`);
+  public getPkmn() :Observable<any> {
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1050`);
   }
+
+  public getIdPkmn() :Observable<any> {
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/`)
+  }
+
 }
