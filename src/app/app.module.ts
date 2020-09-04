476553d1bfedd212a896b3a997591d8c050df975
Matheus Cabral <matheuscabraloli@gmail.com>
2020-09-04 14:46:23 -0300

    Ajuste Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { MaterialModule } from './shared/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
-import { PokeTableComponent } from './components/poke-table/poke-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ListaPokemonComponent } from './components/lista-pokemon/lista-pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PokeTableComponent,
-    PokeDetailComponent,
-    FooterComponent
+    FooterComponent,
+    ListaPokemonComponent
   ],
   imports: [
     BrowserModule,
