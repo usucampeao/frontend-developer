526474a408666a75c6abd0d9b2a49b23905d51d0
Matheus Cabral <matheuscabraloli@gmail.com>
2020-09-05 16:50:16 -0300

    Lista + Header

--- src/app/components/lista-pokemon/lista-pokemon.component.ts
+++ src/app/components/lista-pokemon/lista-pokemon.component.ts
@@ -1,7 +1,13 @@
-import { Router } from '@angular/router';
-import { Pokemons } from './../../models/placeholder.model';
+import { Pokemon } from './../../models/pokemon';
 import { PokemonService } from './../../services/pokemon.service';
 import { Component, OnInit } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-lista-pokemon',
@@ -9,32 +15,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-pokemon.component.scss']
 })
 export class ListaPokemonComponent implements OnInit {
-  pokemons = new Pokemons;
-  erro: any;
+  
+  /**
+   * The component maintains
+   * a list of Pokemon objects
+   * that will be rendered.
+   *
+   * We initialize it to an empty
+   * list.
+   */
+  pokemon: Pokemon[] = [];
 
+  /**
+   * A boolean that represents
+   * if we are currently loading data.
+   */
+  isLoading: boolean = false;
 
-  constructor(private pokeService: PokemonService, private router: Router) {
-    this.getter();
+  /**
+   * This boolean will be set
+   * to true if an error occurred.
+   */
+  error: boolean = false;
+
+  constructor(private pokeService: PokemonService) {
   }
 
-  ngOnInit(): void {}
-
-  getter() {
-    this.pokeService.getPkmn().subscribe(
-      (data: Pokemons) => {
-        this.pokemons = data;
-        // console.log('Varável recebida',data);
-        console.log('Varável preenchida',this.pokemons);
-        console.log(this.pokemons.results.length);
-      },
-      (error: any) => {
-        this.erro = error;
-      }
-    );
+  ngOnInit(): void {
+    /**
+     * Load the initial data.
+     */
+    this.loadMore();
+  }
+  
+  loadMore() {
+    this.isLoading = true;
+    
+    /**
+     * Use the Pokedex service
+     * to load the next 9 Pokemon.
+     */
+    this.pokeService.getPokemon(this.pokemon.length, 25)
+    .then(pokemon => {
+      pokemon = pokemon.map(p => {
+        p.imageLoaded = false;
+        return p;
+      });
+      /**
+       * If loading was successful
+       * we append the result to the list.
+       */
+      this.pokemon = this.pokemon.concat(pokemon);
+      this.isLoading = false;
+      this.error = false;
+      })
+      .catch(() => {
+        this.error = true;
+        this.isLoading = false;
+      });
   }
 
-  getRow(row) {
-    this.router.navigateByUrl(`pokeDetail/${row.position}`);
-  }
+  
+
+  // getter() {
+  //   this.pokeService.getPkmn().subscribe(
+  //     (data: Pokemons) => {
+  //       this.pokemons = data;
+  //       // console.log('Varável recebida',data);
+  //       console.log('Varável preenchida',this.pokemons);
+  //       console.log(this.pokemons.results.length);
+  //     },
+  //     (error: any) => {
+  //       this.erro = error;
+  //     }
+  //   );
+  // }
+
+  // getRow(row) {
+  //   this.router.navigateByUrl(`pokeDetail/${row.position}`);
+  // }
 
 }
